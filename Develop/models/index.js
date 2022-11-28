bf21e047dd61976f4808c2d78f45235c691e03d6
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-11-27 20:32:31 -0600

    index model many-to-many relationships

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -15,9 +15,21 @@ Category.hasMany(Product, {
   onDelete: 'SET NULL'
 })
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'product_id'
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'tag_id'
+})
 module.exports = {
   Product,
   Category,
