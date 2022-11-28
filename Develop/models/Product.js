d6eedbdade0be1f169eff26b4f3336facd1ec792
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-11-27 19:23:12 -0600

    product model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -26,7 +26,17 @@ Product.init({
     isDecimal: true,
   },
   stock: {
-
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    isNumeric: true
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'category',
+      key: 'id'
+    }
   },
     sequelize,
     timestamps: false,
