8900611340ca100439c3c0b47404363b90a4db0b
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-11-27 20:08:32 -0600

    index model relationships

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,9 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+})
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
